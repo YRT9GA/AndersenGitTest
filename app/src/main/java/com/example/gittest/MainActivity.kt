6e803d411895a5730191e849e16d9e1c47539579
YRT9GA <yrt12345@gmail.com>
2022-01-12 10:56:18 +0300

    Added logs (homework 2)

--- app/src/main/java/com/example/gittest/MainActivity.kt
+++ app/src/main/java/com/example/gittest/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.gittest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.util.Log.ASSERT
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("MainActivity", "Test debug")
+        Log.e("MainActitviry", "Test error")
+        Log.i("MainActitviry", "Test info")
+        Log.v("MainActitviry", "Test info")
+        Log.w("MainActitviry", "Test info")
     }
 }
